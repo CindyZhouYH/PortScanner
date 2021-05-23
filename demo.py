1f2c49da6b1eb41332f27a3c63a76c238f9937ba
周远航 <18373004@buaa.edu.cn>
2021-05-23 13:53:35 +0800

    add report

--- demo.py
+++ demo.py
@@ -2,7 +2,9 @@ from Scanner import PortScanner
 
 scanner = PortScanner.PostScanner(timeout=10, required_num=100, target=None, thread_num_bound=100)
 
-host_name = '10.136.211.23'
+# host_name = '49.232.136.182'
+# host_name = '47.94.243.9'
+host_name = '192.168.75.1'
 
 message = 'hello from CindyZhou'
 
